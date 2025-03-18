a5bcabfc4a48baac9978f345c59e38deb47af3bc
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-03-18 15:16:40 +0600

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Customer = () => {
+    return (
+        <div>
+            <p>Customer page</p>
+        </div>
+    );
+};
+
+export default Customer;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const invoice = () => {
+    return (
+        <div>
+            <p>Invoice page</p>
+        </div>
+    );
+};
+
+export default invoice;
